f85d310297772b2025b181165419a7063d57544a
bbejeck <bbejeck@gmail.com>
2020-10-03 14:06:40 -0400

    started config of schemaRegistry plugin, added initial Avro files and code for schema migrations, improved logging

---
+++ src/main/java/bbejeck/utils/Schemas.java
@@ -0,0 +1,14 @@
+package bbejeck.utils;
+
+/**
+ * User: Bill Bejeck
+ * Date: 10/2/20
+ * Time: 2:25 PM
+ */
+public class Schemas {
+    //final String schemaRegistryUrl;
+
+    private Schemas() {
+
+    }
+}

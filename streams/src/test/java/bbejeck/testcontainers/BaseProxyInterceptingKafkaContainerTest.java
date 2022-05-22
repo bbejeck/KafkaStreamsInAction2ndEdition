a81eca6fa36dc7fd93f164fe93cbd003eff69156
bbejeck <bbejeck@gmail.com>
2022-05-22 12:43:41 -0400

    Updated for AK 3.2, log4j settings for testcontainers to turn down logging, toxiproxy image for M1 mac and detecting which image to use

--- streams/src/test/java/bbejeck/testcontainers/BaseProxyInterceptingKafkaContainerTest.java
+++ streams/src/test/java/bbejeck/testcontainers/BaseProxyInterceptingKafkaContainerTest.java
@@ -7,6 +7,8 @@ import org.testcontainers.containers.Network;
 import org.testcontainers.containers.ToxiproxyContainer;
 import org.testcontainers.utility.DockerImageName;
 
+import java.util.Properties;
+
 /**
  * Base Kafka container for re-use across tests
  * This class makes use of the Toxiproxy container
@@ -17,18 +19,25 @@ public class BaseProxyInterceptingKafkaContainerTest {
     private static final Logger LOG = LogManager.getLogger(BaseProxyInterceptingKafkaContainerTest.class);
     private static final String TOXIPROXY_NETWORK_ALIAS = "toxiproxy";
     private static final ToxiproxyContainer TOXIPROXY;
-    private static final DockerImageName TOXIPROXY_IMAGE = DockerImageName.parse("shopify/toxiproxy:2.1.0");
-    
+    private static final DockerImageName TOXIPROXY_AMD64_IMAGE = DockerImageName.parse("ghcr.io/shopify/toxiproxy:v2.4-amd64").asCompatibleSubstituteFor("shopify/toxiproxy");
+    private static final DockerImageName TOXIPROXY_X86_IMAGE = DockerImageName.parse("ghcr.io/shopify/toxiproxy:v2.4").asCompatibleSubstituteFor("shopify/toxiproxy");
+
     public static final KafkaContainer KAFKA;
     public static final ToxiproxyContainer.ContainerProxy PROXY;
 
     static {
+
+        Properties properties = System.getProperties();
+        String os = properties.getProperty("os.arch", "");
         Network network = Network.newNetwork();
         KAFKA = new ProxyInterceptingKafkaContainer(DockerImageName.parse("confluentinc/cp-kafka:7.1.0"))
                 .withExposedPorts(9093)
                 .withNetwork(network);
 
-        TOXIPROXY = new ToxiproxyContainer(TOXIPROXY_IMAGE)
+        DockerImageName toxiProxyImage = os.equals("aarch64") ? TOXIPROXY_AMD64_IMAGE : TOXIPROXY_X86_IMAGE;
+        LOG.info("Using ToxiProxy image {}", toxiProxyImage);
+
+        TOXIPROXY = new ToxiproxyContainer(toxiProxyImage)
                 .withNetwork(network)
                 .withNetworkAliases(TOXIPROXY_NETWORK_ALIAS);
 

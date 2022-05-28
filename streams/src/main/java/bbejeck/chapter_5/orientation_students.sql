ecf6c91d3259ce83057346a77cc5bc9585af443c
bbejeck <bbejeck@gmail.com>
2022-05-28 15:11:22 -0400

    Get source connector working with minimum settings

--- streams/src/main/java/bbejeck/chapter_5/orientation_students.sql
+++ streams/src/main/java/bbejeck/chapter_5/orientation_students.sql
@@ -2,19 +2,34 @@ DROP TABLE IF EXISTS orientation_students;
 
 CREATE TABLE orientation_students
 (
-    id         SERIAL PRIMARY KEY,
     ssn        VARCHAR(11)  NOT NULL,
-    stu_name   VARCHAR(255) NOT NULL,
+    email      VARCHAR(255) NOT NULL,
+    user_name  VARCHAR(255) NOT NULL,
+    full_name  VARCHAR(255) NOT NULL,
     address    VARCHAR(255) NOT NULL,
     state      VARCHAR(255) NOT NULL,
     major      VARCHAR(255) NOT NULL,
     overnight  CHARACTER DEFAULT 'N',
-    ts         timestamp DEFAULT NOW()
+    ts         TIMESTAMP DEFAULT NOW() NOT NULL 
 );
 
-INSERT INTO orientation_students VALUES (DEFAULT, '123-02-9865', 'Art Vandelay', '123 Beach Drive, Hamptons', 'NY', 'Architecture', 'Y');
-INSERT INTO orientation_students VALUES (DEFAULT, '331-02-2865', 'Betty Rubble', '405 Elm Street, Rockville', 'MD', 'Finance', 'N');
-INSERT INTO orientation_students VALUES (DEFAULT, '435-12-3865', 'Stephen Strange', '177 A Bleeker Street, New York CIty', 'NY', 'Mystic Arts', 'Y');
+
+CREATE OR REPLACE FUNCTION update_ts_column_on_update()
+    RETURNS TRIGGER AS $$
+BEGIN
+    NEW.ts = now();
+    RETURN NEW;
+END;
+$$ language 'plpgsql';
+
+
+CREATE TRIGGER update_orientation_students_timestamp BEFORE UPDATE
+    ON orientation_students FOR EACH ROW EXECUTE PROCEDURE
+    update_ts_column_on_update();
+
+INSERT INTO orientation_students VALUES ('123-02-9865', 'vandelay@gmail.com', 'artv', 'Art Vandelay', '123 Beach Drive, Hamptons', 'NY', 'Architecture', 'Y');
+INSERT INTO orientation_students VALUES ('331-02-2865', 'brubble@hotmail.com', 'bettyrub', 'Betty Rubble', '405 Elm Street, Rockville', 'MD', 'Finance', 'N');
+INSERT INTO orientation_students VALUES ('435-12-3865', 'sstrange@marvel.com', 'timemaster', 'Stephen Strange', '177 A Bleeker Street, New York CIty', 'NY', 'Mystic Arts', 'Y');
 
 
 

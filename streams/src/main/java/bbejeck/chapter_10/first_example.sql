604309fd6dc7e9f74aeec60950ce8f89cc396ca9
bbejeck <bbejeck@gmail.com>
2023-02-20 13:38:21 -0500

    docker compose files for ksql arm64 and x86 first example sql file

---
+++ streams/src/main/java/bbejeck/chapter_10/first_example.sql
@@ -0,0 +1,21 @@
+-- noinspection SqlNoDataSourceInspectionForFile
+from terminal window docker exec -it ksqldb-cli ksql http://ksqldb-server:8088
+from IntelliJ terminal  ksql http://ksqldb-server:8088
+
+CREATE STREAM input_stream (phrase VARCHAR) WITH (kafka_topic='src-topic', partitions=1, value_format='KAFKA');
+
+CREATE STREAM yelling AS
+SELECT UCASE(phrase) AS SHOUT
+FROM input_stream
+    EMIT CHANGES;
+
+SET 'auto.offset.reset' = 'earliest';
+
+INSERT INTO input_stream (phrase) VALUES ('Chuck Norris finished World of Warcraft');
+INSERT INTO input_stream (phrase) VALUES ('Chuck Norris first program was kill -9');
+INSERT INTO input_stream (phrase) VALUES ('generate bricks-and-clicks content');
+INSERT INTO input_stream (phrase) VALUES ('brand best-of-breed infomediaries');
+
+
+SELECT * FROM yelling;
+

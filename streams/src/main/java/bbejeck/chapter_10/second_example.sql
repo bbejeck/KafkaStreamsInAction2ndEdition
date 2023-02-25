193a250804d87b2fca7841e81684b3a085cb0ec6
bbejeck <bbejeck@gmail.com>
2023-02-25 15:01:56 -0500

    Added commands for 2nd round of ksqlDB examples

--- streams/src/main/java/bbejeck/chapter_10/second_example.sql
+++ streams/src/main/java/bbejeck/chapter_10/second_example.sql
@@ -1,8 +1,11 @@
 -- noinspection SqlNoDataSourceInspectionForFile
 
-from terminal window docker exec -it ksqldb-cli ksql http://ksqldb-server:8088
-from IntelliJ terminal  ksql http://ksqldb-server:8088
+-- instructions to set up ksqlCLI
+-- start docker compose with file appropriate for your chip architecture
+-- then from terminal window docker exec -it ksqldb-cli ksql http://ksqldb-server:8088
+-- or from IntelliJ terminal  ksql http://ksqldb-server:8088
 
+-- run this statement to create the stream
 CREATE STREAM user_activity (first_name VARCHAR,
                              last_name VARCHAR,
                              activity VARCHAR,
@@ -15,6 +18,19 @@ CREATE STREAM user_activity (first_name VARCHAR,
     timestamp = 'event_time',
     timestamp_format = 'yyyy-MM-dd HH:mm:ss'
 );
+-- insert statements to get started to run queries
+INSERT INTO user_activity (first_name, last_name, activity, event_time, steps ) VALUES ('Art', 'Vandelay', 'walking', FORMAT_TIMESTAMP(FROM_UNIXTIME(UNIX_TIMESTAMP()), 'yyyy-MM-dd HH:mm:ss'), 200);
+INSERT INTO user_activity (first_name, last_name, activity, event_time, steps ) VALUES ('Mike', 'Rogers', 'running', FORMAT_TIMESTAMP(FROM_UNIXTIME(UNIX_TIMESTAMP() + (60 * 1000 * 2)), 'yyyy-MM-dd HH:mm:ss'), 10000);
+INSERT INTO user_activity (first_name, last_name, activity, event_time, steps ) VALUES ('Bruce', 'Banner', 'hulk-smash', FORMAT_TIMESTAMP(FROM_UNIXTIME(UNIX_TIMESTAMP() + (60 * 1000 * 3)), 'yyyy-MM-dd HH:mm:ss'), 300);
+INSERT INTO user_activity (first_name, last_name, activity, event_time, steps ) VALUES ('Art', 'Vandelay', 'cycling', FORMAT_TIMESTAMP(FROM_UNIXTIME(UNIX_TIMESTAMP() + (60 * 1000 * 4)), 'yyyy-MM-dd HH:mm:ss'), 200);
+INSERT INTO user_activity (first_name, last_name, activity, event_time, steps ) VALUES ('Mike', 'Rogers', 'running', FORMAT_TIMESTAMP(FROM_UNIXTIME(UNIX_TIMESTAMP() + (60 * 1000 * 5)), 'yyyy-MM-dd HH:mm:ss'), 20000);
+INSERT INTO user_activity (first_name, last_name, activity, event_time, steps ) VALUES ('Jane', 'Smith', 'cycling', FORMAT_TIMESTAMP(FROM_UNIXTIME(UNIX_TIMESTAMP() + (60 * 1000 * 6)), 'yyyy-MM-dd HH:mm:ss'), 20000);
+INSERT INTO user_activity (first_name, last_name, activity, event_time, steps ) VALUES ('Jane', 'Smith', 'walking', FORMAT_TIMESTAMP(FROM_UNIXTIME(UNIX_TIMESTAMP() + (60 * 1000 * 7)), 'yyyy-MM-dd HH:mm:ss'), 2000);
+INSERT INTO user_activity (first_name, last_name, activity, event_time, steps ) VALUES ('Steve', 'Jones', 'lifting', FORMAT_TIMESTAMP(FROM_UNIXTIME(UNIX_TIMESTAMP() + (60 * 1000 * 8)), 'yyyy-MM-dd HH:mm:ss'), 400);
+INSERT INTO user_activity (first_name, last_name, activity, event_time, steps ) VALUES ('Mike', 'Rogers', 'sprinting', FORMAT_TIMESTAMP(FROM_UNIXTIME(UNIX_TIMESTAMP() + (60 * 1000 * 9)), 'yyyy-MM-dd HH:mm:ss'), 5000);
+INSERT INTO user_activity (first_name, last_name, activity, event_time, steps ) VALUES ('John', 'Hechinger', 'walking', FORMAT_TIMESTAMP(FROM_UNIXTIME(UNIX_TIMESTAMP()  + (60 * 1000 * 10)), 'yyyy-MM-dd HH:mm:ss'), 900);
+
+SET 'auto.offset.reset' = 'earliest';
 
 CREATE TABLE activity_leaders AS
 SELECT
@@ -22,7 +38,9 @@ SELECT
     SUM(steps)
 FROM user_activity
 GROUP BY last_name 
-         EMIT CHANGES;
+EMIT CHANGES;
+
+DROP TABLE activity_leaders;
 
 CREATE TABLE activity_leaders WITH (KEY_FORMAT = 'JSON') AS
 SELECT
@@ -36,3 +54,16 @@ SELECT
     FROM user_activity
     GROUP BY first_name, last_name, activity
     EMIT CHANGES;
+
+-- Example of a push query run then CTRL+C to stop query
+SELECT
+  last_name, activity, total_steps
+FROM activity_leaders
+WHERE total_steps > 1000
+EMIT CHANGES;
+
+-- Example of a pull query
+SELECT last_name, activity, total_steps
+FROM activity_leaders
+WHERE key_2 = 'Smith';
+

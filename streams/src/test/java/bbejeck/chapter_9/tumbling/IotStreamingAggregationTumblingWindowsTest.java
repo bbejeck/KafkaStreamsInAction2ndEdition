f9d3143732ca2e60802aef208641443cbab27d33
Bill Bejeck <bbejeck@gmail.com>
2024-03-11 15:29:07 -0400

    Remove unused import in test file
    Removed an unused import statement, `import static java.time.LocalDate.EPOCH;` from the `IotStreamingAggregationTumblingWindowsTest` file. This change aligns to clean code practices and helps maintain a lower number of unused imports in the codebase.

--- streams/src/test/java/bbejeck/chapter_9/tumbling/IotStreamingAggregationTumblingWindowsTest.java
+++ streams/src/test/java/bbejeck/chapter_9/tumbling/IotStreamingAggregationTumblingWindowsTest.java
@@ -20,7 +20,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Properties;
 
-import static java.time.LocalDate.EPOCH;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class IotStreamingAggregationTumblingWindowsTest {

3ab5cfc1a688b0c626d7f97b0e52125370fcddc5
bbejeck <bbejeck@gmail.com>
2024-09-11 12:14:53 -0400

    Upgrade Kafka test container to use KRaft mode.
    This change updates the KafkaContainer to start with KRaft mode enabled, improving the testing setup by utilizing the latest Kafka architecture. The modification ensures compatibility with newer Kafka features and configurations.

--- streams/src/test/java/bbejeck/testcontainers/BaseKafkaContainerTest.java
+++ streams/src/test/java/bbejeck/testcontainers/BaseKafkaContainerTest.java
@@ -14,7 +14,7 @@ public abstract class BaseKafkaContainerTest {
     }
 
     static {
-        KAFKA = new KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka:7.5.1"));
+        KAFKA = new KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka:7.5.1")).withKraft();
         KAFKA.start();
     }
     
